472c82adf3afa955290738ef980592ba6b1bcb10
Libebathi <102481590+LibenathiMlindi@users.noreply.github.com>
2022-06-24 16:33:03 +0200

    Update TriangleAreaCircumference.php
    Final commit

--- TriangleAreaCircumference.php
+++ TriangleAreaCircumference.php
@@ -33,4 +33,34 @@ $a = 2; $b = 2; $c = 3;
 // Find the radius of the incircle
 echo findRadiusOfIncircle($a, $b, $c) . "\n";
 
+// Findinding Surface Area
+      
+function findArea( $r)
+{
+    $PI =3.142;
+    return $PI * pow($r, 0.566947);
+}
+  
+// Driver Code
+echo("Area is ");
+echo(findArea(5));
+return 0;
+
+// Finding circumference of circle
+ 
+$PI= 3.1415;
+ 
+function circumference($r)
+{
+    global $PI;
+    $cir = 2 * $PI * $r;
+    return $cir;
+}
+ 
+// Driver Code
+$r = 0.566947;
+echo "Circumference = ",
+circumference($r);
+ 
+
 ?>
